14841a538e2c000221dcec218dd79b3c3a7a9733
Sergio <sps169@outlook.es>
2021-11-09 00:19:52 +0100

    Markdown generated correctly

--- src/main/java/ioutils/JDOM.java
+++ src/main/java/ioutils/JDOM.java
@@ -1,26 +1,33 @@
 package ioutils;
 
 import org.jdom2.Document;
+
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
+import org.jdom2.filter.Filters;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
-import pojos.Measure;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+import pojos.CityMeans;
+import pojos.DataMean;
+import service.Analytics;
 
+import javax.xml.bind.JAXBException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class JDOM{
     private Document document;
     private final String URI;
-    private List<Measure> measureList;
 
     public JDOM (String uri) {
         this.URI = uri;
@@ -33,7 +40,7 @@ public class JDOM{
 
     private void fillDocumentDataFromXML() throws IOException, JDOMException {
         SAXBuilder saxBuilder = new SAXBuilder();
-        this.document = saxBuilder.build(this.URI);
+        this.document = saxBuilder.build(new File(this.URI));
     }
 
     private XMLOutputter getPrettyOutputter () {
@@ -54,6 +61,27 @@ public class JDOM{
     public void initDocument() {
         this.document = new Document();
         this.document.setProperty("Charset", "windows-1252");
+    }
+
+    public List<CityMeans> getAnalyticsOfCity (String city) throws XPathExpressionException {
+        XPathFactory pathFactory = XPathFactory.instance();
+        XPathExpression<Element> xpathQuery = pathFactory.compile("/database/informs/inform[station/stationCity = \""+city+"\"]", Filters.element());
+        return xpathQuery.evaluate(this.document).stream().map(s -> {
+            return new CityMeans(
+                    city,
+            s.getChild("contamination").getChildren("contaminationData").stream().map(t ->
+                    new DataMean(
+                            Double.parseDouble(t.getChildText("meanValueOfMeasures")) ,
+                            t.getChild("type").getChildText("description")
+                    )
+            ).collect(Collectors.toList()),
+            s.getChild("meteorology").getChildren("meteorologyData").stream().map(t ->
+                    new DataMean(
+                            Double.parseDouble(t.getChildText("meanValueOfMeasures")) ,
+                            t.getChild("type").getChildText("description")
+                    )
+            ).collect(Collectors.toList()));
+        }).collect(Collectors.toList());
     }
 
     public Document getDocument() {
---
+++ src/main/java/ioutils/MarkDownGenerator.java
@@ -0,0 +1,23 @@
+package ioutils;
+
+import pojos.CityMeans;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class MarkDownGenerator {
+    public static void generateMarkdownOfCityMeans(List<CityMeans> means, String uri) throws IOException {
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < means.size(); i++) {
+            buffer.append(means.get(i).toString());
+        }
+        if (!Files.exists(Path.of(uri))) {
+            Files.createFile(Path.of(uri));
+        }
+        Files.writeString(Path.of(uri), buffer, Charset.forName("windows-1252"));
+    }
+}
